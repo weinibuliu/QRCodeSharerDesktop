90245a5a8fc03ca438811d5a6a8e4b8919b75a9d
weinibuliu <weinibuliu@outlook.com>
2025-12-27 23:46:26 +0800

    feat: use FluentAvalonia

--- QRCodeSharer.Desktop/App.axaml.cs
+++ QRCodeSharer.Desktop/App.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using FluentAvalonia.Styling;
 using QRCodeSharer.Desktop.Views;
 
 namespace QRCodeSharer.Desktop;
@@ -10,6 +11,10 @@ public partial class App : Application
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+        
+        // 使用 FluentAvalonia 主题
+        var faTheme = new FluentAvaloniaTheme();
+        Styles.Add(faTheme);
     }
 
     public override void OnFrameworkInitializationCompleted()
